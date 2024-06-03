c81ad785b9663a4ae106f86e34f42eed9992b298
DeepakSingh260 <alexmha260@gmail.com>
2024-06-03 08:25:52 +0530

    layout fix

--- app/(tabs)/Home/index.tsx
+++ app/(tabs)/Home/index.tsx
@@ -1,12 +1,22 @@
 import ChatBotScreen from "./messaging";
 import React from "react";
+import { StyleSheet } from "react-native";
 import { useState } from "react";
 import { View } from "react-native";
 export default function HomeScreen ()  {
   const [ showMessage, setShowMessage ] = useState(true);
   return (
-    <View>
+    <View style={styles.container}>
       {showMessage && <ChatBotScreen />}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  chatBot: {
+    flex: 1,
+  },
+});
